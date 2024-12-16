2f0292453103d1bfa58cf3b0f233092f9c44fce4
Rishabh Singh <Rishabh.home.1@gmail.com>
2024-12-15 22:05:28 -0600

    STAGE FRIGHT

--- stagefreight/src/routes/+layout.svelte
+++ stagefreight/src/routes/+layout.svelte
@@ -98,7 +98,7 @@
       </p>
     </div>
     <div class="text-xs sm:text-sm text-center lg:text-left sm:block">
-      <p class="hidden sm:block">Rishabh Singh | Kaushal Ganapur | Aadwaith Sajin</p>
+      <p class="hidden sm:block">Rishabh Singh | Kaushal Ganapur</p>
     </div>
   </footer>
 </section>
